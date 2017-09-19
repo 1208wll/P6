72b194ae881aeeba41802c3567f7acba23e4eaba
imweb-next <kuangweike@gmail.com>
2017-09-19 18:04:17 +0800

    clean dist

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 var webpack = require('webpack');
 var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
@@ -22,6 +23,7 @@ module.exports = {
     },
     plugins: [
         new UglifyJsPlugin(),
+        new CleanWebpackPlugin('./dist'),
         new ExtractTextPlugin("styles.css"),
         new HtmlWebpackPlugin({
           filename: 'index.html', // 编译到 dist/index.html
